917efb5672e3b1dbab29de8669718e1e282431a3
Naoyuki Kojima <116235104+ronaudo9@users.noreply.github.com>
2024-03-11 09:38:01 +0900

    ベースデザインの作成（レスポンス対応） (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 export default function Home() {
   return (
-    <h1 className=" bg-red-50 text-3xl font-bold underline">Hello world!</h1>
+    <>
+      <div className="absolute left-5 top-5 h-8 w-36 bg-red-50 bg-no-repeat opacity-100 md:left-52 md:top-5">
+        Travel Blog
+      </div>
+      <header className="absolute left-0 top-20 h-12 w-full bg-green-200"></header>
+      <div className="mt-44 bg-blue-50 md:mx-52">aa</div>
+      <div className="mt-16 grid grid-cols-1 gap-11 md:mx-52 md:grid-cols-3">
+        <div className="bg-yellow-50 md:col-span-2">aa</div>
+        <div className="bg-gray-50 md:col-span-1">aa</div>
+      </div>
+      <footer className="mt-36 bg-green-100">aa</footer>
+    </>
   );
 }
